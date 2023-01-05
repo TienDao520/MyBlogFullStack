9b940855d8ca9ead797e85621834e4cf0fccb556
Tien Dao <tiendao5240@gmail.com>
2023-01-04 23:34:02 -0500

    added store={store}

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ sagaMiddleware.run(mySaga);
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
