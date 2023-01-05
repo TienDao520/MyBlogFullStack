a6edfc92264d86fa12452ae3047085691b507cde
Tien Dao <tiendao5240@gmail.com>
2023-01-04 23:33:11 -0500

    Basic config for redux-saga

--- src/index.js
+++ src/index.js
@@ -3,9 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createSagaMiddleware } from 'redux-saga';
+import reducers from './redux/reducers';
+import mySaga from './redux/sagas';
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(reducers, applyMiddleware(sagaMiddleware));
+
+sagaMiddleware.run(mySaga);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
-  <React.StrictMode>
+  <Provider>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
